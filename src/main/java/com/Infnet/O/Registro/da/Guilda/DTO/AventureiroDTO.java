3e6299f4208e886ec4922007c3cb69bc03f6b0cb
lucasminecraft9999-rgb <lucasminecraft9999@gmail.com>
2026-03-05 15:08:23 -0300

    tp1-feito

---
+++ src/main/java/com/Infnet/O/Registro/da/Guilda/DTO/AventureiroDTO.java
@@ -0,0 +1,18 @@
+package com.Infnet.O.Registro.da.Guilda.DTO;
+
+
+import com.Infnet.O.Registro.da.Guilda.DTO.Enum.ClasseAventureiro;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record AventureiroDTO(
+        Long id,
+       @NotBlank String nome,
+       @NotNull ClasseAventureiro classe ,
+        @NotNull Integer nivel,
+        Boolean ativo,
+       Companheiro companheiro
+) {
+
+}
---
+++ src/test/java/com/Infnet/O/Registro/da/Guilda/ORegistroDaGuildaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Infnet.O.Registro.da.Guilda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ORegistroDaGuildaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
